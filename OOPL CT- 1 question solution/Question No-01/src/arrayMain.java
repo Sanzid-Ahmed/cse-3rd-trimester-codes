8ebc38e44194abf339ed36c6265d3ae1dfe24af8
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-08-25 11:12:02 +0600

    added main array class q - p - 1

---
+++ OOPL CT- 1 question solution/Question No-01/src/arrayMain.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class arrayMain {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int[] array1 = {45, 78, 22, 96, 53, 41, 11, 88};
+
+
+    }
+}
