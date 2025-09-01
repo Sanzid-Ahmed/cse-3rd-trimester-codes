ac4cd059009223c679a844e64a8d91198f044989
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-09-01 17:12:46 +0600

    added 2NO answer 1

---
+++ OOP Mid Question NO 2/02NO Question-01/src/Employee.java
@@ -0,0 +1,24 @@
+public class Employee{
+    public String name;
+    private int employee_id;
+    private double salary;
+// Write constructor to initialize name, employee id and salary with this reference keyword;
+    Employee(String name, int employee_id, double salary){
+        this.name = name;
+        this.employee_id = employee_id;
+        this.salary = salary;
+    }
+// Write getter method for salary;
+    double getSalary(){
+        return salary;
+    }
+// Write setter method for salary;
+    void setSalary(double salary){
+        this.salary = salary;
+    }
+
+public static void main(String[] args) {
+    Employee e1 = new Employee("Prof. Albert Einstein", 19, 2000.50 );
+    System.out.println(e1.name);
+    }
+}
